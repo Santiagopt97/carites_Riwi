b4eae0250e5c616348ace4d1a9dbc74b27537c78
Santiagopt97 <santiagopineda99@gmail.com>
2024-05-23 11:04:33 -0500

    feat: Se crea el main del carrito de comrpas

---
+++ src/js/cart.js
@@ -0,0 +1,42 @@
+const URL = "http://localhost:3000/categories"
+const section = document.querySelector("#section")
+
+async function index() {
+    const response = await fetch(URL)
+    const data = await response.json()
+
+    data.forEach(element => {
+        section.innerHTML += `
+        <article id="borde" class="row g-0 ">
+            <div class="col-md-4 text-center ">
+                <img src="${element.image}" class='img-fluid ms-3 image' alt="${element.name}">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${element.name}</h5>
+                    <p class="card-text">${element.description}</p>
+                    <p class="card-text"><small class="text-body-secondary">${element.price}</p>
+                </div>
+                <div class="d-flex justify-content-between ">
+                    <div>
+                        <td>
+                            <button type="button" data-id=${element.id} class="btn btn-warning">Edit</button>
+                            <button type="button" data-id=${element.id} class="btn btn-danger">Delete</button>
+                        </td>
+                    </div>
+                    
+                    <div>
+                        <td>
+                            <button type="button" data-id=${element.id} class="btn btn-warning">Edit</button>
+                            <button type="button" data-id=${element.id} class="btn btn-danger">Delete</button>
+                        </td>
+                    </div>
+                </div>
+            </div>
+        </article>
+        
+        `
+    })
+}
+
+index()
