9a3686e031fe5bd9942e1b6c41f0cf61c847af72
Santiagopt97 <santiagopineda99@gmail.com>
2024-05-25 00:43:04 -0500

    fix: anclo las cards al json y modifico botones

--- src/js/cart.js
+++ src/js/cart.js
@@ -7,34 +7,30 @@ async function index() {
 
     data.forEach(element => {
         section.innerHTML += `
-        <article id="borde" class="row g-0 ">
-            <div class="col-md-4 text-center ">
-                <img src="${element.linkProductImage}" class='img-fluid ms-3 image' alt="${element.productName}">
-            </div>
-            <div class="col-md-8">
-                <div class="card-body">
-                    <h5 class="card-title">${element.productName}</h5>
-                    <p class="card-text">${element.description}</p>
-                    <p class="card-text"><small class="text-body-secondary">${element.productPrice}</p>
-                </div>
-                <div class="d-flex justify-content-between ">
-                    <div>
-                        <td>
-                            <button type="button" data-id=${element.id} class="btn btn-warning">Edit</button>
-                            <button type="button" data-id=${element.id} class="btn btn-danger">Delete</button>
-                        </td>
+        <article id="article" class="card mb-3 cards">
+                <div id="card-id" class="row g-0 ">
+                    <div id="img-card" class="col-md-4 text-center ">
+                        <img src="${element.linkProductImage}" class='img-fluid ms-3 image' alt="${element.productName}">
                     </div>
-                    
-                    <div>
-                        <td>
-                            <button type="button" data-id=${element.id} class="btn btn-warning">Edit</button>
-                            <button type="button" data-id=${element.id} class="btn btn-danger">Delete</button>
-                        </td>
+                    <div id="text-card" class="col-md-8">
+                        <div class="card-body d-flex flex-column justify-content-center ">
+                            <h2 id="titulo" class="card-title title text-capitalize ">${element.productName}</h2>
+                            <p class="card-text paragraph text-capitalize">${element.productDescription}</p>
+                            <p class="card-text paragraph ">${element.productPrice}</p>
+                        </div>
+                        <div class="d-flex justify-content-between ">
+                            <div>
+                                <button type="button" data-id=${element.id} class="btn btn-warning">Whatsapp</button>
+                                <button type="button" data-id=${element.id} class="btn btn-danger">Telefono</button>
+                            </div>
+                            
+                            <div>
+                                <button type="button" data-id=${element.id} class="btn btn-danger">Delete</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </article>
-        
+              </article>
         `
     })
 }
