a14dfbb20c7eed3a4d881cdabf54ffdac14526f3
Santiagopt97 <santiagopineda99@gmail.com>
2024-05-29 09:05:04 -0500

    fix: se agrego boton cererar sesion en el index

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 const myNav = document.querySelector("#navbar");
+const containerUser = document.querySelector(".position-a")
 
 window.onscroll = function () {
   if (window.scrollY > window.innerHeight) {
@@ -7,32 +8,18 @@ window.onscroll = function () {
     myNav.classList.remove("scrolled");
   }
 };
-document.addEventListener('', () => {
-  const loginButton = document.getElementById('loginButton');
-  const registerButton = document.getElementById('registerButton');
-  const logoutButton = document.getElementById('logoutButton');
 
-  // Comprobar si hay un usuario en el localStorage
-  const user = JSON.parse(localStorage.getItem('userOnline'));
-
-  if (user) {
-      // Si el usuario está loggeado, ocultar los botones de login y register, y mostrar el de logout
-      loginButton.style.display = 'none';
-      registerButton.style.display = 'none';
-      logoutButton.style.display = 'block';
-  } else {
-      // Si el usuario no está loggeado, mostrar los botones de login y register, y ocultar el de logout
-      loginButton.style.display = 'block';
-      registerButton.style.display = 'block';
-      logoutButton.style.display = 'none';
-  }
-
-  // Agregar el evento de logout
-  logoutButton.addEventListener('click', () => {
-      // Eliminar la información del usuario del localStorage
-      localStorage.removeItem('userOnline');
-      
-      // Redirigir al usuario a la página de inicio de sesión
-      window.location.href = './login.html'; // Ajusta la ruta según tu estructura de archivos
-  });
-});
+window.addEventListener("load", () => {
+  // Recuperamos el item del localStorage
+  const userOnline = localStorage.getItem("userOnline");
+  if (userOnline !== null) {
+    containerUser.innerHTML = `
+    <button type="button" id="logout-button" class="register " data-bs-toggle="modal"
+    on>Cerrar Sesión</button>`
+    let logout = document.querySelector("#logout-button")
+      logout.addEventListener("click", () => {
+        localStorage.removeItem("userOnline");
+        location.reload();
+      })
+    }
+  })
