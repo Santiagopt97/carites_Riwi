7cf6ba65a5853087f5ccf81c91f51baf0a08cba0
Santiagopt97 <santiagopineda99@gmail.com>
2024-05-28 22:11:36 -0500

    fix: solucion de conflictos en database

--- src/js/products.js
+++ src/js/products.js
@@ -14,16 +14,16 @@ async function index(sectionProducts) {
         sectionProducts.innerHTML += `
             <div class="card-product">
                 <div class="face-card front-card">
-                    <img class="img-card-product" src="${product.sale.linkProductImage}" alt="">
-                    <h3 class="h3-card-product">${product.sale.productName}</h3>
+                    <img class="img-card-product" src="${product.linkProductImage}" alt="">
+                    <h3 class="h3-card-product">${product.productName}</h3>
                 </div>
                 <div class="face-card back-card" id="info-back-product">
                         <div>
-                            <h3 class="h3-card-product" id="h3-card-product">${product.sale.productName}</h3>
-                            <p class="p-card-product">${product.sale.productDescription}</p>
+                            <h3 class="h3-card-product" id="h3-card-product">${product.productName}</h3>
+                            <p class="p-card-product">${product.productDescription}</p>
                         </div>
                         <div class="price-info">
-                            <h3 class="h3-card-product"><span>$ </span>${product.sale.productPrice}<span> COP </span></h3>
+                            <h3 class="h3-card-product"><span>$ </span>${product.productPrice}<span> COP </span></h3>
                             <p class="p-card-product">Kilo</p>
                         </div>
                         <div class="price-info">
@@ -38,10 +38,10 @@ async function index(sectionProducts) {
     document.querySelectorAll('.details-btn').forEach(button => {
         button.addEventListener('click', function () {
             const product = JSON.parse(this.getAttribute('data-product'));
-            document.getElementById('modal-product-image').src = product.sale.linkProductImage;
-            document.getElementById('modal-product-name').innerText = product.sale.productName;
-            document.getElementById('modal-product-description').innerText = product.sale.productDescription;
-            document.getElementById('modal-product-price').innerText = `$ ${product.sale.productPrice} COP`;
+            document.getElementById('modal-product-image').src = product.linkProductImage;
+            document.getElementById('modal-product-name').innerText = product.productName;
+            document.getElementById('modal-product-description').innerText = product.productDescription;
+            document.getElementById('modal-product-price').innerText = `$ ${product.productPrice} COP`;
             document.getElementById('modal-owner-name').innerText = product.ownerName;
             document.getElementById('modal-owner-lastname').innerText = product.ownerLastName;
             document.getElementById('modal-owner-town').innerText = product.ownerTown;
