a8cfaa42536f232086bed77f09589c3d1e4f317c
Santiagopt97 <santiagopineda99@gmail.com>
2024-05-29 09:18:55 -0500

    feat: se aggrega boton para ir a usuario

--- src/js/main.js
+++ src/js/main.js
@@ -14,12 +14,18 @@ window.addEventListener("load", () => {
   const userOnline = localStorage.getItem("userOnline");
   if (userOnline !== null) {
     containerUser.innerHTML = `
-    <button type="button" id="logout-button" class="register " data-bs-toggle="modal"
+    <button type="button" id="user-button"  data-bs-toggle="modal"
+    on>Usuario</button>
+    <button type="button" id="logout-button"  data-bs-toggle="modal"
     on>Cerrar Sesión</button>`
     let logout = document.querySelector("#logout-button")
       logout.addEventListener("click", () => {
         localStorage.removeItem("userOnline");
         location.reload();
       })
+    let userButton = document.querySelector("#user-button")
+    userButton.addEventListener("click", () => {
+      window.location.href = "./src/pages/users.html"
+    })
     }
   })
