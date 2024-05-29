e4b479a5bc8e95b4519a4cb70a68d9591cf23180
Karina P <karina_pineda97@hotmail.com>
2024-05-28 22:44:21 -0500

    fix: added function to attach the logged-in user to the product via localstorage

--- src/js/user.js
+++ src/js/user.js
@@ -1,65 +1,76 @@
 const tbody = document.querySelector('#productList')
-const form=document.querySelector('#productForm')
-const productName=document.querySelector('#productName')
-const productPrice=document.querySelector('#productPrice')
-const productQuantity=document.querySelector('#productQuantity')
-const linkProductImage=document.querySelector('#productImageURL')
-const productDescription=document.querySelector('#productDescription')
+const form = document.querySelector('#productForm')
+const productName = document.querySelector('#productName')
+const productPrice = document.querySelector('#productPrice')
+const productQuantity = document.querySelector('#productQuantity')
+const linkProductImage = document.querySelector('#productImageURL')
+const productDescription = document.querySelector('#productDescription')
 
-const URL_PRODUCT='http://localhost:3000/agriculturalProducts/'
+const URL_PRODUCT = 'http://localhost:3000/agriculturalProducts/'
 let id
 
 index()
-form.addEventListener('submit',async (event)=>{
+form.addEventListener('submit', async (event) => {
     event.preventDefault()
-    if(!id){
+    if (!id) {
         await create(productName.value, productPrice.value, productQuantity.value, linkProductImage.value, productDescription.value)
-    } else{
-        await updateProduct(id,productName.value, productPrice.value, productQuantity.value, linkProductImage.value,productDescription.value)
-        id=undefined
+    } else {
+        await updateProduct(id, productName.value, productPrice.value, productQuantity.value, linkProductImage.value, productDescription.value)
+        id = undefined
     }
     await index()
     form.reset()
 })
 
-tbody.addEventListener('click',async (event)=>{
-    if(event.target.classList.contains('btn-danger')){
+tbody.addEventListener('click', async (event) => {
+    if (event.target.classList.contains('btn-danger')) {
         const id = event.target.getAttribute('data-id')
         await deleteProduct(id)
         await index()
-    }else if (event.target.classList.contains('btn-warning')){
-        id= event.target.getAttribute('data-id')
-        const productFound=await find(id)
-        productName.value=productFound.productName
-        productPrice.value=productFound.productPrice
-        productQuantity.value=productFound.productQuantity
-        linkProductImage.value=productFound.linkProductImage
-        productDescription.value=productFound.productDescription
+    } else if (event.target.classList.contains('btn-warning')) {
+        id = event.target.getAttribute('data-id')
+        const productFound = await find(id)
+        productName.value = productFound.productName
+        productPrice.value = productFound.productPrice
+        productQuantity.value = productFound.productQuantity
+        linkProductImage.value = productFound.linkProductImage
+        productDescription.value = productFound.productDescription
     }
 })
 
-async function find(id){
-    const response = await fetch(URL_PRODUCT+id)
+async function find(id) {
+    const response = await fetch(URL_PRODUCT + id)
     const data = await response.json()
     return data
 }
 
-async function create(productName, productPrice, productQuantity, linkProductImage, productDescription){
-    const newProducts={productName, productPrice, productQuantity, linkProductImage, productDescription}
-    await fetch(URL_PRODUCT,{
-        method:'POST',
-        body:JSON.stringify(newProducts),
-        headers:{
-            'Content-Type':'application/json'
+async function create(productName, productPrice, productQuantity, linkProductImage, productDescription) {
+    const user = JSON.parse(localStorage.getItem('userOnline'))
+    const newProducts = {
+        productName, productPrice, productQuantity, linkProductImage, productDescription, ownerAgricola: {
+            ownerName: user.ownerName,
+            ownerLastName: user.ownerLastName,
+            ownerDocument: user.ownerDocument,
+            ownerTown: user.ownerTown,
+            ownerPhoneNumber: user.ownerPhoneNumber,
+            ownerEmail: user.ownerEmail,
+            ownerNumberWhatsapp: user.ownerNumberWhatsapp
+        }
+    }
+    await fetch(URL_PRODUCT, {
+        method: 'POST',
+        body: JSON.stringify(newProducts),
+        headers: {
+            'Content-Type': 'application/json'
         },
     })
 }
 
-async function index(){
-    const response  = await fetch(URL_PRODUCT)
+async function index() {
+    const response = await fetch(URL_PRODUCT)
     const data = await response.json()
-    
-    tbody.innerHTML=''
+
+    tbody.innerHTML = ''
     data.forEach(product => {
         tbody.innerHTML += `
         <td>${product.productName}</td>
@@ -77,18 +88,18 @@ async function index(){
     })
 }
 
-async function updateProduct(id,productName,productPrice,productQuantity,linkProductImage,productDescription){
-    await fetch(URL_PRODUCT+id,{
+async function updateProduct(id, productName, productPrice, productQuantity, linkProductImage, productDescription) {
+    await fetch(URL_PRODUCT + id, {
         method: 'PUT',
-        body:JSON.stringify({productName,productPrice,productQuantity,linkProductImage,productDescription}),
-        headers:{
-            'Content-Type':'application/json'
+        body: JSON.stringify({ productName, productPrice, productQuantity, linkProductImage, productDescription }),
+        headers: {
+            'Content-Type': 'application/json'
         }
     })
 }
 
-async function deleteProduct(id){
-    await fetch(URL_PRODUCT+id,{
-        method:'DELETE'
+async function deleteProduct(id) {
+    await fetch(URL_PRODUCT + id, {
+        method: 'DELETE'
     })
 }
